93f38653296c067f06ca43482fe735ff25f29b92
shadowswind <731246251@qq.com>
2018-09-23 15:33:14 +0800

    forth commit

--- main/main.js
+++ main/main.js
@@ -1,4 +1,26 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+function rtrim(str){ 
+    return str.replace(/(\s*$)/g,"");
+}
+module.exports = function main(sting) {
+    var lcdTotal = [
+        ['._. ','|.| ','|_| '],
+        ['... ','..| ','..| '],
+        ['._. ','._| ','|_. '],
+        ['._. ','._| ','._| '],
+        ['... ','|_| ','..| '],
+        ['._. ','|_. ','._| '],
+        ['._. ','|_. ','|_| '],
+        ['._. ','..| ','..| '],
+        ['._. ','|_| ','|_| '],
+        ['._. ','|_| ','..| ']];
+    var result = '';
+    for (var i = 0; i<3; i++){
+        for (var num of sting){
+            num = Number(num);
+            result += lcdTotal[num][i];
+        }
+        result = rtrim(result);
+        result += '\n';
+    }
+        return result;
 };
